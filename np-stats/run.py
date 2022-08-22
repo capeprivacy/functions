abf76c49c76c85f69fb5034ae3f93fe5e6278958
yanndupis <yann.dupis@gmail.com>
2022-08-22 10:58:02 -0700

    Update numpy example

--- np-stats/run.py
+++ np-stats/run.py
@@ -2,18 +2,27 @@ import os
 
 import numpy as np
 from pycape import Cape
+from pycape import FunctionRef
 
 import numpy_serde as serde
 
 if __name__ == "__main__":
     token = os.environ.get("CAPE_TOKEN", None)
-    url = os.environ.get("CAPE_HOST", "wss://hackathon.capeprivacy.com")
-    function_id = os.environ.get(
-        "CAPE_FUNCTION", "b65b77c4-5422-451b-8291-55d9f4b7a016"
+    url = os.environ.get("CAPE_HOST", "wss://enclave.capeprivacy.com")
+    function_id = os.environ.get("CAPE_FUNCTION_ID", "bQyv2fbtyJukqx6fUhxMpW")
+    function_hash = os.environ.get(
+        "CAPE_FUNCTION_HASH",
+        "d66591e203325d3fec3dc9d6f5936a419adf3a373014f99cd65ec56bbfe4da4a",
     )
+
+    if function_hash is None:
+        function_ref = function_id
+    else:
+        function_ref = FunctionRef(function_id, function_hash)
 
     cape = Cape(url=url, access_token=token)
     x = np.array([1, 2, 3, 4])
-    result = cape.run(function_id, x, serde_hooks=(serde.encoder, serde.decoder), use_serdio=True)
+    f = FunctionRef(function_id, function_hash)
+    result = cape.run(f, x, serde_hooks=(serde.encoder, serde.decoder), use_serdio=True)
 
     print(f"The result is: {result}")
