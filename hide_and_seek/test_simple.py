e5416bf0227a002348bc13b9692dc951c6cbeb95
Eric <eric@capeprivacy.com>
2022-11-24 14:15:40 -0500

    too sensitive

--- hide_and_seek/test_simple.py
+++ hide_and_seek/test_simple.py
@@ -9,13 +9,15 @@ function_json = os.environ.get("FUNCTION_JSON", "seek.json")
 function_json = pathlib.Path(__file__).parent.absolute() / function_json
 function_ref = FunctionRef.from_json(function_json)
 
-data = {"latitude":"44.646461","longitude":"-63.593312"}
-user_data = bytes(json.dumps(data), "utf8")
+data1 = {"latitude":"43.795315","longitude":"-79.3240118"}
+user_data1 = bytes(json.dumps(data1), "utf8")
+data2 = {"latitude":"43.7939503","longitude":"-79.3224387"}
+user_data2 = bytes(json.dumps(data2), "utf8")
 cape = Cape()
 cape.connect(function_ref)
 
-result = cape.invoke(user_data)
+result = cape.invoke(user_data1)
 print(result)
-result = cape.invoke(user_data)
+result = cape.invoke(user_data2)
 print(result)
 cape.close()
