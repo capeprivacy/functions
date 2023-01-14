45ec708a0fb94f6cab68450e196a601f9604f76f
Eric <eric@capeprivacy.com>
2023-01-14 00:51:34 -0500

    refactor ocr function for large enclave

--- ocr/app.py
+++ ocr/app.py
@@ -1,16 +1,12 @@
-import os
-import pathlib
+import io
 import ocrmypdf
 import sys
 
 
-deploy_dir = os.environ.get("DEPLOY_DIR") or pathlib.Path(__file__).parent
 sys.stdout = open('/dev/null', 'w')
 def cape_handler(pdf_bytes: bytes):
-    with open("my_file.pdf", "wb") as binary_file:
-        binary_file.write(pdf_bytes)
-    # doc = doctr.io.DocumentFile.from_pdf(pdf_bytes)
-    ocrmypdf.ocr(input_file="my_file.pdf", output_file='-', output_type='none', sidecar='output.txt', force_ocr=True)
+    pdf = io.BytesIO(pdf_bytes)    
+    ocrmypdf.ocr(input_file=pdf, output_file='-', output_type='none', sidecar='output.txt', force_ocr=True)
     with open("output.txt", "rb") as f:
         result = f.read()
         return result
